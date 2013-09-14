b1dcfadd9447dfa33ff8c0aa7de93fb15b5afd79
Brendon Murphy <xternal1+github@gmail.com>
2013-09-14 00:43:41 -0700

    Add error handling to the form

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,28 @@
 $('#new-job-posting').on('submit', function(e){
   e.preventDefault();
 
+  var form = $(this);
+  var errClass= 'has-error';
+
   var success = function(data) {
     window.location = "/thanks";
   };
 
+  $('.form-group').removeClass(errClass);
+
   var error = function(xhr, status, error) {
-    console.log(error);
-    console.log(xhr.responseJSON);
+    var obj = xhr.responseJSON;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        form.find('[name="' + key + '"]')
+          .closest('.form-group')
+          .addClass(errClass);
+      }
+    }
   };
 
-  var formData = $(this).serialize();
+  var formData = form.serialize();
 
   $.ajax({
     type: 'POST',
